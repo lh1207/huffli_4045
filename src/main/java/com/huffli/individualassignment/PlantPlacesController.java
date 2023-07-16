afd147aba30f24e52989f4a88325fb4dddf990fc
Levi H <100445409+lh1207@users.noreply.github.com>
2023-07-16 19:16:59 -0400

    modify PlantPlacesController and welcome page

--- src/main/java/com/huffli/individualassignment/PlantPlacesController.java
+++ src/main/java/com/huffli/individualassignment/PlantPlacesController.java
@@ -1,12 +1,71 @@
-package com.huffli.individualassignment;
+package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
 
 @Controller
 public class PlantPlacesController {
-	@GetMapping("/")
-	public String welcome() {
-		return "welcome";
+	
+	@Autowired
+	private ISpecimenService specimenServiceStub;
+
+	/**
+	 * Handle the /start endpoint.
+	 * @return
+	 */
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read(Model model) {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		model.addAttribute("specimenDTO", specimenDTO);
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, headers={"content-type=text/json"})
+	public String readJSON() {
+		return "start";
+	}
+	
+	/**
+	 * Handle the /start endpoint.
+	 * @return
+	 */
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params={"loyalty=blue"})
+	public String readBlue() {
+		return "start";
+	}
+	
+	/**
+	 * Handle the /start endpoint.
+	 * @return
+	 */
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params={"loyalty=silver"})
+	public String readSilver() {
+		return "start";
+	}
+	
+	
+	@PostMapping("/start")
+	public String create() {
+		return "start";
+	}
+	
+	/**
+	 * Handle the / endpoint.
+	 * @return
+	 */
+		
+	@RequestMapping("/")
+	public String index() {
+		return "start";
 	}
 }
